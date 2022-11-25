756e822bf79d1899959e1bf0f5b326c75ec2f8f7
iglesiasmaxwell <iglesias.maxwell@gmail.com>
2022-11-25 09:21:59 +0800

    update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,9 @@
 <div class="flex-1 bg-blue-300 py-4 px-6 uppercase">
     <div class="flex flex-row gap-8">
-        <a class="hover:ring-sky-600 ring-1 hover:text-white px-4 py-2 rounded-md cursor-pointer" href="/blog">
+        <a class="hover:ring-sky-800 ring-1 hover:text-white px-4 py-2 rounded-md cursor-pointer" href="/blog">
             Konten
         </a>
-        <a class="hover:ring-sky-600 ring-1 hover:text-white px-4 py-2 rounded-md cursor-pointer" href="{{ route('crud.index') }}">
+        <a class="hover:ring-sky-800 ring-1 hover:text-white px-4 py-2 rounded-md cursor-pointer" href="{{ route('crud.index') }}">
             {{-- href="/crud/index" --}}
             Crud
         </a>
